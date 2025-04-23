b792bba147b4e598e3de9837122315f06a91b51d
nasrinnahardisha <nasrinnahardisha@gmail.com>
2025-04-23 12:23:03 +0600

    rrf-template-3

--- src/js/script.js
+++ src/js/script.js
@@ -10,98 +10,82 @@ menuBtn.addEventListener("click", () => {
 });
 
 
+const links = document.querySelectorAll('.nav-link');
 
-// accordian
-const headers = document.querySelectorAll(".accordion-header");
-
-headers.forEach(header => {
-  header.addEventListener("click", () => {
-    const body = header.nextElementSibling;
-    const icon = header.querySelector("svg");
-
-    document.querySelectorAll(".accordion-body").forEach(item => {
-      if (item !== body) {
-        item.classList.add("hidden");
-      }
-    });
-
-    document.querySelectorAll(".accordion-header svg").forEach(svg => {
-      if (svg !== icon) {
-        svg.classList.remove("rotate-180");
-      }
-    });
-
-    body.classList.toggle("hidden");
-    icon.classList.toggle("rotate-180");
+links.forEach(link => {
+  link.addEventListener('click', function () {
+    links.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
   });
 });
 
 
 
 
-$(document).ready(function ($) {
-
-  
-  $('.project-slide').slick({
-    slidesToShow: 1,
-    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-arrow-left text-2xl text-[#c9c9c9]"></i> </button>',
-    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-right text-2xl text-[#c9c9c9]"></i></button>',
-    appendArrows: $('#custom-arrows'),
-    autoplay: true,
-    autoplaySpeed: 3000,
-  });
 
 
 
 
-  $('.team-slider').slick({
-    slidesToShow: 3,
+
+
+$(document).ready(function () {
+  $(".testimonial-content").slick({
     arrows: false,
     dots: true,
     autoplay: true,
-   autoplaySpeed: 3000, 
-   responsive: [
-     {
-       breakpoint: 1024, 
-       settings: {
-         slidesToShow: 2,
-       }
-     },
-     {
-       breakpoint: 768, 
-       settings: {
-         slidesToShow: 1,
-       }
-     }
-   ]
-  });
-
-
-  $('.story-slider').slick({
-    slidesToShow: 1,
-    arrows: false,
-    autoplay: true,
     autoplaySpeed: 3000,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   });
-
-})
-
+});
 
 
-let activeIndex = null;
 
-function toggleAccordion(index) {
-    if (activeIndex !== null && activeIndex !== index) {
-        document.getElementById(`content-${activeIndex}`).classList.add('hidden');
-        document.getElementById(`icon-${activeIndex}`).textContent = '+';
-    }
 
-    const content = document.getElementById(`content-${index}`);
-    const icon = document.getElementById(`icon-${index}`);
 
-    const isHidden = content.classList.contains('hidden');
-    content.classList.toggle('hidden');
-    icon.textContent = isHidden ? '–' : '+';
 
-    activeIndex = isHidden ? index : null;
-}
+
+
+
+
+
+var normalMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Â© OpenStreetMap contributors'
+});
+
+var satelliteMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/' +
+  'World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles Â© Esri'
+});
+
+
+var map = L.map('map', {
+  center: [23.8948, 90.4086],
+  zoom: 13,
+  layers: [normalMap]
+});
+
+
+
+var baseMaps = {
+  "Normal Map": normalMap,
+  "Satellite Map": satelliteMap
+};
+L.control.layers(baseMaps).addTo(map);
+
+
+var customIcon = L.divIcon({
+  className: "",
+  html: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="40" viewBox="0 0 26 40" fill="none">
+      <path d="M13.1481 0.306519C6.3979 0.306519 0.922852 6.20125 0.922852 13.4718C0.922852 17.1933 4.28906 23.8571 4.28906 23.8571L12.6983 39.3226L21.4705 24.0363C21.4705 24.0363 25.3681 17.6788 25.3681 13.4718C25.3671 6.20125 19.8962 0.306519 13.1481 0.306519ZM13.0906 20.7254C9.22539 20.7254 6.09455 17.3294 6.09455 13.1338C6.09455 8.94501 9.22539 5.55018 13.0906 5.55018C16.9536 5.55018 20.0887 8.94388 20.0887 13.1338C20.0887 17.3294 16.9536 20.7254 13.0906 20.7254Z" fill="#FF0000"/>
+    </svg>
+  `,
+  iconSize: [26, 40],
+  iconAnchor: [13, 40] 
+});
+
+
+L.marker([23.8948, 90.4086], { icon: customIcon }).addTo(map)
+  .bindPopup("Tongi, Gazipur");
